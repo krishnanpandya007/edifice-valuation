05366472cf942e6de0bfade1f3f1015ef9c623a0
krishnanpandya007 <krishnanpandya06@gmail.com>
2025-05-19 11:23:18 +0530

    fix:PWA 4

--- valuation_site/urls.py
+++ valuation_site/urls.py
@@ -22,8 +22,8 @@ from django.urls import path, include
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("core.urls")),
     path("", include('pwa.urls')),
+    path("", include("core.urls")),
     # path('', index, name='index')
 ]
 
